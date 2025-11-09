01ae1ba50e0b3ea388d4870a3cf8a4c7d2e737dc
Michael Donovan <32551579+MDoN65@users.noreply.github.com>
2025-11-09 00:34:44 -0400

    .

--- hand.gd
+++ hand.gd
@@ -5,6 +5,10 @@ extends Node3D
 
 func _ready() -> void:
 	cigarette.visible = show_cigarette
+	# Set multiplayer authority to match the parent player
+	var player = get_parent()
+	if player and player.has_method("get_multiplayer_authority"):
+		set_multiplayer_authority(player.get_multiplayer_authority())
 
 @rpc("authority")
 func set_show_cigarette(value: bool) -> void:
@@ -12,8 +16,9 @@ func set_show_cigarette(value: bool) -> void:
 	show_cigarette = value
 	if is_instance_valid(cigarette):
 		cigarette.visible = value
-	# Broadcast the new value to all remote peers
-	rpc("_rpc_update_show_cigarette", value)
+	# Only broadcast if we are the authority
+	if is_multiplayer_authority():
+		rpc("_rpc_update_show_cigarette", value)
 
 @rpc("call_remote")
 func _rpc_update_show_cigarette(value: bool) -> void:
